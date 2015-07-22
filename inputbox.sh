49699584e6d679b8536eaa61e03ba10cc634ca55
alxbuilder <alxbuilder@gmail.com>
2015-07-22 14:20:38 +0100

    code cleaned up

--- inputbox.sh
+++ inputbox.sh
@@ -1,49 +1,7 @@
 #!/bin/bash
 #(@) Whiptail inputbox
 
-function length() {
-        expr `echo "$*" |wc -c` - 1 
-}
-
-function longest() {
-        LONGEST=0
-        for STRING in $*
-        do  
-                THISLEN=`length $STRING`
-                [ $THISLEN -gt $LONGEST ] && \
-                        LONGEST=$THISLEN
-        done
-        echo $LONGEST
-}
-
-function snake_case() {
-	echo $* |sed -e s/\ /_/g
-}
-
-function rows() {
-	echo `tput cols`
-}
-
-function columns() {
-	echo `tput lines`
-}
-
-function max_width() {
-	echo `rows - 6`	# whiptail boxes use 3 chars before & after text
-}
-
-function screen_size() {
-	ROWS=`tput cols` && COLS=`tput lines` && RESULT="${ROWS}x${COLS}"
-	echo $RESULT
-}
-
-function too_long() {
-	[ `length $*` -gt `max_width` ]
-}
-
-function truncate() {
-	echo $* |cut -c1-$(expr `rows` - 6)
-}
+. ./functions
 
 # Try and streamline this stuff
 MESSAGE="Enter something, but should this text determine box width? \
@@ -51,20 +9,11 @@ MESSAGE="Enter something, but should this text determine box width? \
 TITLE="Some complicated activity that should probably be automated for \
 the likes of Jane and other such people"
 
-# if too_long $TITLE
-# then
-# 	TITLE=`truncate $TITLE`
-# fi
-
+# Truncate the title if it's too big for the window
 [ too_long $TITLE ] && TITLE=`truncate $TITLE`]
 LENGTH=`max_width`
 
-LONGEST=$(longest `snake_case $TITLE` `snake_case $MESSAGE`)
-# LENGTH=$LONGEST
-
-# Add 3 chars each side to make box look right
-# LENGTH=`expr $LENGTH + 6`
-
+# Produce our dialog box
 SOMETHING=$(whiptail 				\
 		--title		"$TITLE"	\
 		--inputbox	"$MESSAGE"	\
