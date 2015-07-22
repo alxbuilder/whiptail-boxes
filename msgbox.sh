840565f2adc64f866e5de6a2e5c406f62294fb6a
alxbuilder <alxbuilder@gmail.com>
2015-07-22 15:59:04 +0100

    msgbox.sh added

---
+++ msgbox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#(@) Whiptail inputbox
+
+. ./functions
+
+# Try and streamline this stuff
+MESSAGE="Enter something, but should this text determine box width? \
+  It is probably a better idea to use the title alone to determine this"
+TITLE="Some complicated activity that should probably be automated for \
+people of lesser technical ability"
+
+# Truncate the title if it's too big for the window
+[ too_long $TITLE ] && TITLE=`truncate $TITLE`]
+LENGTH=`max_width`
+
+# Produce our dialog box
+USERINPUT=$(whiptail 				\
+		--title		"$TITLE"	\
+		--msgbox	"$MESSAGE"	\
+		8 $LENGTH 3>&1 1>&2 2>&3)
+echo You entered \"${USERINPUT}\"
