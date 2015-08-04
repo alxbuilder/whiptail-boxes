873dce0a6f14b725da11d513623f7cd42a10b4b6
alxbuilder <alxbuilder@gmail.com>
2015-08-04 10:18:16 +0100

    Fixed "truncate" namespace problem

--- msgbox.sh
+++ msgbox.sh
@@ -6,11 +6,11 @@
 # Sample data to illustrate what problem this solves.
 MESSAGE="This is a longwinded message to the user, to demonstrate \
   that only the '--title' should be used to determine the box size"
-TITLE="This title will be truncated if not kept short enough to fit in \
+TITLE="This title will be shortend if not kept short enough to fit in \
   a dialog box that the screen can display"
 
 # Truncate the title if it's too big for the window
-[ too_long $TITLE ] && TITLE=`truncate $TITLE`]
+[ too_long $TITLE ] && TITLE=`shorten $TITLE`]
 LENGTH=`max_width`
 
 # Produce our dialog box
--- yesnobox.sh
+++ yesnobox.sh
@@ -1,1 +1,25 @@
-whiptail --title "Some box" --yesno "Some instruction" --fb --ok-button "Allright" --cancel-button "Uh-ohh" --yes-button "yes please" --no-button "go away"  10 50 "replace this"
+#!/bin/bash
+#(@) Whiptail inputbox
+
+. ./functions
+
+# Sample data to illustrate what problem this solves.
+MESSAGE="This is a longwinded message to the user, to demonstrate \
+  that only the '--title' should be used to determine the box size"
+TITLE="This title will be shortend if not kept short enough to fit in \
+  a dialog box that the screen can display"
+
+# Truncate the title if it's too big for the window
+[ too_long $TITLE ] && TITLE=`shorten $TITLE`]
+LENGTH=`max_width`
+
+# This looks like a good idea
+BOXTYPE="--yesno"
+read anykey
+# RESULT=$(
+whiptail --title "$TITLE" $BOXTYPE "$MESSAGE" \
+	--fb --ok-button "Allright" --cancel-button "Uh-ohh" \
+	--yes-button "yes please" --no-button "go away" \
+	10 50 "replace this" # )
+
+# echo $RESULT
