add044f64342f3bfb5d6f523f2616624aaf57aea
alxbuilder <alxbuilder@gmail.com>
2015-08-05 14:43:32 +0100

    detected

--- checklist.sh
+++ checklist.sh
@@ -3,12 +3,26 @@
 
 . ./functions
 
+# Sample data to illustrate what problem this solves.
+BOXTYPE="--checklist"
+MESSAGE="This is a longwinded message to the user, to demonstrate \
+  that only the '--title' should be used to determine the box size"
+TITLE="This title will be shortend if not kept short enough to fit in \
+  a dialog box that the screen can display"
+
+# Truncate the title if it's too big for the window
+[ `too_long "$TITLE"` ] && TITLE=`shorten $TITLE`
+LENGTH=`max_width`
+
+# Produce our dialog box
 #	--backtitle	"Any explanatory message can be put here"	\
+#	--scrolltext	\
+
 whiptail_menu() {
 	whiptail 			\
-	--title		"Select one of" \
-	--checklist	"Checklist Selector" \
-	13 40 5 \
+	--title		"$TITLE" \
+	$BOXTYPE	"$MESSAGE" \
+	19 40 $MENUHEIGHT \
 		"1st"	"task 1"	off	\
 		"default" "task 2"	on	\
 		"3rd"	"task 3"	off	\
@@ -20,9 +34,15 @@ whiptail_menu() {
 		"9th"	"task 9"	off	\
 		"10th"	"task 10"	off	\
 		--default-item	"default"	\
-		--scrolltext	\
+		--fb	\
 	3>&1 1>&2 2>&3
 }
+
+MENUHEIGHT=$(grep '$BOXTYPE' -A 20 $0 |grep -B 20 '3>&1' |grep "\".*\".*\".*\"" |grep -v MENU |wc -l )
+# echo MENUHEIGHT is $MENUHEIGHT
+MENUCONTENT=$(grep '$BOXTYPE' -A 20 $0 |grep -B 20 '3>&1' |grep "\".*\".*\".*\""|grep -v MENU )
+# echo $MENUCONTENT
+# exit
 
 TASK=`whiptail_menu`
 echo $TASK
